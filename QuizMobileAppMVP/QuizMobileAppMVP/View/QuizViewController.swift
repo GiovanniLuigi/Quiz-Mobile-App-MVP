9590d4b43e81dcce32b8883d7df1f96d3fadadd1
Giovanni Bruno <giovannibruno128@gmail.com>
2019-11-05 08:37:36 -0300

    Project setup

---
+++ QuizMobileAppMVP/QuizMobileAppMVP/View/QuizViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizMobileAppMVP
+//
+//  Created by Giovanni Bruno on 05/11/19.
+//  Copyright © 2019 Giovanni Bruno. All rights reserved.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
