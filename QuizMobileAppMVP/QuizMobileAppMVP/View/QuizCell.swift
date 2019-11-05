13c3c9c778280755f13eb1b7130117604cb5a80f
Giovanni Bruno <giovannibruno128@gmail.com>
2019-11-05 10:48:47 -0300

    Setup WordsTableView

---
+++ QuizMobileAppMVP/QuizMobileAppMVP/View/QuizCell.swift
@@ -0,0 +1,18 @@
+//
+//  QuizCell.swift
+//  QuizMobileAppMVP
+//
+//  Created by Giovanni Bruno on 05/11/19.
+//  Copyright © 2019 Giovanni Bruno. All rights reserved.
+//
+
+import UIKit
+
+class QuizCell: UITableViewCell {
+    
+    static let reuseIdentifier: String = "wordCell"
+    
+    @IBOutlet weak var wordLabel: UILabel!
+    
+    
+}
--- QuizMobileAppMVP/QuizMobileAppMVP/View/QuizViewController.swift
+++ QuizMobileAppMVP/QuizMobileAppMVP/View/QuizViewController.swift
@@ -9,13 +9,42 @@
 import UIKit
 
 class QuizViewController: UIViewController {
+    // MARK: - Outlets
     @IBOutlet weak var wordTextField: UITextField! 
+    @IBOutlet weak var wordsTableView: UITableView!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var timerLabel: UILabel!
     
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupWordsTableView()
     }
-
-
+    
+    // MARK: - Actions
+    @IBAction func didTapStartButton(_ sender: Any) {
+    }
+    
+    // MARK: - Private functions
+    private func setupWordsTableView() {
+        wordsTableView.dataSource = self
+    }
 }
 
+
+// MARK: - WordsTableView
+extension QuizViewController: UITableViewDataSource  {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: QuizCell.reuseIdentifier) as? QuizCell {
+            cell.wordLabel.text = "static"
+            return cell
+        } else {
+            return UITableViewCell()
+        }
+    }
+    
+}
